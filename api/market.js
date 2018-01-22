2fe4338c009cc849a28076329a334ca4171661c4
srjahir32 <srjahir32@gmail.com>
2018-01-22 11:28:54 +0530

    Update for api

--- api/market.js
+++ api/market.js
@@ -4,28 +4,36 @@ var client = new Client();
 var pool = require('../config/db.js');
 
 
-exports.getMarket=function(req,res) {
-	// body...
-	console.log('get market is called')
+pool.connect(function(db) {
+    if (db) {
+        console.log('connected');
+    } else {
+        console.log('Error');
+    }
+})
+
+exports.getMarket = function(req, res) {
+    // body...
+    console.log('get market is called')
 }
 cron.schedule('0 */1 * * * *', function() {
-	test();
-	// getMarketData();
-}); 
-function test(){
-	console.log('Cron runnig');
+    // test();
+    // getMarketData();
+});
+
+function test() {
+    console.log('Cron runnig');
 }
 
-function getMarketData(){
-	console.log('saving market data into database')
-	client.get("https://www.cryptopia.co.nz/api/GetMarkets", function (data, response) {
-		if(data.Success==true){
-			for(var i=0;i<data.Data.length;i++){
-				console.log(data.Data[i]);	
-			}
-		}
-	    else{
-	    	console.log('there is no any data found')
-	    }
-	});
+function getMarketData() {
+    console.log('saving market data into database')
+    client.get("https://www.cryptopia.co.nz/api/GetMarkets", function(data, response) {
+        if (data.Success == true) {
+            for (var i = 0; i < data.Data.length; i++) {
+                console.log(data.Data[i]);
+            }
+        } else {
+            console.log('there is no any data found')
+        }
+    });
 }
--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
-var mongoUrl = process.env.MONGODB_URI;
-
+var mongoUrl = "mongodb://127.0.0.1:27017/admin";
 exports.connect = function(callback) {
     MongoClient.connect(mongoUrl, function(err, db) {
         if (err) {
-            console.log('Connection Error ',err);
+            console.log('Connection Error ', err);
             callback(false);
         } else {
             callback(db);
@@ -13,3 +12,28 @@ exports.connect = function(callback) {
         }
     });
 };
+
+
+// var mongodb = require('mongodb');
+// var MongoClient = mongodb.MongoClient;
+// // var mongoUrl = 'mongodb://heroku_bvm7pktg:curq73l0i58mhtvaq9a961sgc2@ds157584.mlab.com:57584/heroku_bvm7pktg';
+// //console.log('mongoUrl', mongoUrl);
+// exports.connect = function(callback) {
+//     MongoClient.connect(mongoUrl, function(err, db) {
+//         if (err) {
+//             callback(false);
+//         } else {
+//             users = db.collection('users');
+//             images = db.collection('images');
+//             settings = db.collection('settings');
+//             connections = db.collection('connections');
+//             chatroom = db.collection('chatroom');
+//             report = db.collection('report');
+//             male = db.collection('male');
+//             female = db.collection('female');
+
+//             callback(db);
+//             // db.close();
+//         }
+//     });
+// };
